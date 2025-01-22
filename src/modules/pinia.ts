096c081f515043abed2ebb8ef2b119d1d3b7e20a
wb_yu_bian <wb_yu_bian@dgyiheda.com>
2025-01-22 09:30:08 +0800

    Refactor project structure and remove unused components
    - Removed unnecessary components and layouts
    - Updated README.md
    - Modified main application files and styles

--- src/modules/pinia.ts
+++ src/modules/pinia.ts
@@ -1,12 +1,17 @@
 import type { UserModule } from '~/types'
 import { createPinia } from 'pinia'
-import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
+// import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
+import { createPersistedState } from 'pinia-plugin-persistedstate'
 
 // Setup Pinia
 // https://pinia.vuejs.org/
 export const install: UserModule = ({ isClient, initialState, app }) => {
   const pinia = createPinia()
-  pinia.use(piniaPluginPersistedstate)
+  // pinia.use(piniaPluginPersistedstate)
+  pinia.use(createPersistedState({
+    storage: sessionStorage,
+    key: id => `__persisted__${id}`,
+  }))
   app.use(pinia)
 
   // Refer to
--- src/stores/test.ts
+++ src/stores/test.ts
@@ -1,19 +1,19 @@
 export const useTestStore = defineStore('test', () => {
-  const test = [
-    { name: '流程推算', path: '/workflow', icon: 'i-carbon:buoy' },
-    { name: '项目跟踪', path: '/tracking', icon: 'i-carbon:buoy' },
-    { name: '项目跟踪2', path: '/tracking3', icon: 'i-carbon:buoy' },
-    { name: '项目跟踪3', path: '/tracking3', icon: 'i-carbon:buoy' },
-    { name: '项目跟踪4', path: '/tracking4', icon: 'i-carbon:buoy' },
-    { name: '项目跟踪5', path: '/tracking4', icon: 'i-carbon:buoy' },
-    { name: '项目跟踪6', path: '/tracking4', icon: 'i-carbon:buoy' },
-    { name: '项目跟踪7', path: '/tracking4', icon: 'i-carbon:buoy' },
-    { name: '项目跟踪8', path: '/tracking4', icon: 'i-carbon:buoy' },
-  ]
+  const test = ref([
+    { label: '测试0', value: 'value-1' },
+    { label: '测试1', value: 'value-2' },
+    { label: '测试2', value: 'value-3' },
+    { label: '测试3', value: 'value-4' },
+    { label: '测试4', value: 'value-5' },
+    { label: '测试5', value: 'value-6' },
+    { label: '测试6', value: 'value-7' },
+    { label: '测试7', value: 'value-8' },
+    { label: '测试8', value: 'value-9' },
+    { label: '测试9', value: 'value-10' },
+
+  ])
   return { test }
-}, { persist: {
-  storage: sessionStorage,
-} })
+}, { persist: true })
 
 if (import.meta.hot)
   import.meta.hot.accept(acceptHMRUpdate(useTestStore, import.meta.hot))
